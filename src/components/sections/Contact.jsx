25b5d2918c6f133cddab46608cd10be8c9c6d31c
kurtpatrickyu <92959247+kurtpatrickyu@users.noreply.github.com>
2025-08-05 17:49:11 +0800

    Fixed env

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -7,10 +7,8 @@ export const Contact = () => {
         name: "",
         email: "",
         message: "",
-    })
-    console.log(import.meta.env.VITE_SERVICE_ID);
-    console.log(import.meta.env.VITE_TEMPLATE_ID);
-    console.log(import.meta.env.VITE_PUBLIC_KEY);
+    });
+    
     const handleSubmit = (e) => {
         e.preventDefault();
 
